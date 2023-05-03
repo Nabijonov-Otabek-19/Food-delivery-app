26e6f0210693ccb0d5941b86862c7246cc49513e
DAVID <nabijonovo23@gmail.com>
2023-05-03 17:06:41 +0300

    project updated

---
+++ app/src/main/java/uz/gita/fooddeliveryapp_bek/data/common/DrinkData.kt
@@ -0,0 +1,7 @@
+package uz.gita.fooddeliveryapp_bek.data.common
+
+data class DrinkData(
+    val title: String,
+    val description: String,
+    val image: String
+)
---
+++ app/src/main/java/uz/gita/fooddeliveryapp_bek/data/common/FoodData.kt
@@ -0,0 +1,7 @@
+package uz.gita.fooddeliveryapp_bek.data.common
+
+data class FoodData(
+    val title: String,
+    val description: String,
+    val image: String
+)
